50c765af6f47983d02ac9089b866cf5f29ae5f68
Taylor Iverson <iversont2@students.westerntc.edu>
2026-04-10 08:27:59 -0500

    Angular- everything but the pages

---
+++ Blumenstein-MovieLibrary/Client-Angular/src/app/components/movie-card/movie-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-card',
+  imports: [],
+  templateUrl: './movie-card.html',
+  styleUrl: './movie-card.css',
+})
+export class MovieCard {}
---
+++ Blumenstein-MovieLibrary/Client-Angular/src/app/components/movies/movies.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movies',
+  imports: [],
+  templateUrl: './movies.html',
+  styleUrl: './movies.css',
+})
+export class Movies {}
