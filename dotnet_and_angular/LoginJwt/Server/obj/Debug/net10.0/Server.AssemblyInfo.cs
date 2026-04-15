ce195e27cdc77cb6f703c116819924f47d532e81
t-blumenstein <iversont2@students.westerntc.edu>
2026-04-14 22:18:12 -0500

    started assignment 3

--- dotnet_and_angular/LoginJwt/Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
+++ dotnet_and_angular/LoginJwt/Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfbe5b45c307ad9fb0891ecfc412e4c05a667527")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f506e4ee9c71edb48553bb93faf46d001732d8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
